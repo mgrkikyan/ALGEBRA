103af1e34a9e6443ac42243bd4c83f563f5afce5
Мисак Гркикян <110992990+mgrkikyan@users.noreply.github.com>
2023-12-08 17:42:56 +0400

    lab2

--- lab2/app2.py
+++ lab2/app2.py
@@ -1,38 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Координаты точки
-x0, y0 = -4, -3
+# Задаем точку A и вектор u
+A = np.array([-4, -3])
+u = np.array([7, 1])
 
-# Координаты вектора
-a, b = 7, 1
+# Вычисляем координаты конечной точки прямой
+B = A + u
 
-# Параметрическое уравнение прямой
-t = np.linspace(-1, 1, 400)
-x = x0 + a * t
-y = y0 + b * t
+# Вычисляем нормальный вектор к прямой
+n = np.array([-u[1], u[0]])
 
-plt.scatter(x0, y0, color='red', alpha=1)
-plt.text(x0, y0 - 0.15, 'A', fontsize=12, ha='right')
+# Вычисляем коэффициенты уравнения прямой y = kx + b
+k = u[1] / u[0]
+b = A[1] - k * A[0]
 
-# Вектор нормали
-normal = np.array([-b, a])
-normal = normal / np.linalg.norm(normal)
+# Создаем фигуру и оси
+fig, ax = plt.subplots()
 
-# График
-plt.plot(x, y, label="Прямая")
-plt.arrow(x0, y0-0.5, a, b, head_width=0.1, head_length=0.15, fc='blue', ec='blue', label="Направляющий вектор")
-plt.arrow(x0, y0, normal[0], normal[1], head_width=0.15, head_length=0.15, fc='red', ec='red', label="Вектор нормали")
+# Рисуем прямую, проходящую через точку A в направлении вектора u
+ax.plot([A[0], B[0]], [A[1], B[1]], 'r', label='Прямая')
 
-# Подписи осей и векторов
-plt.xlabel('x')
-plt.ylabel('y')
+# Рисуем нормальный вектор
+ax.arrow(A[0], A[1], n[0], n[1], head_width=0.5, head_length=0.5, fc='b', ec='b', label='Нормаль')
+
+# Подписываем оси
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+
+# Подписываем точку A и векторы u и n
+ax.annotate('A', A, textcoords="offset points", xytext=(-10,-10), ha='center')
+ax.annotate('u', B, textcoords="offset points", xytext=(10,10), ha='center')
+ax.annotate('n', A + n, textcoords="offset points", xytext=(-10,10), ha='center')
+
+# Выводим уравнение прямой
+print(f'Уравнение прямой: y = {k}x + {b}')
+
+# Выводим график
 plt.legend()
-
-# Вывод уравнения прямой
-print(f'Уравнение прямой: (x - ({x0})) / {a} = (y - ({y0})) / {b}')
-
-# Показать график
+plt.grid(True)
 plt.show()
-
-
